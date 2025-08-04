9da7b42b7cef2e68f9e86c2e28e63e35d2c633b4
Tarannum Parwin <tarannumparwin1995@gmail.com>
2025-08-04 12:14:53 +0530

    Added Streamlit app and model

--- app.py
+++ app.py
@@ -1,42 +1,32 @@
+# app.py
 import streamlit as st
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import mnist
 
 # Title
 st.title("🧠 Handwritten Digit Recognizer")
 st.write("This app predicts digits (0–9) using a trained neural network.")
 
-# Load and normalize data
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-y_train_cat = to_categorical(y_train)
-y_test_cat = to_categorical(y_test)
-
-# Train and cache model
+# Load the pre-trained model
 @st.cache_resource
-def train_model():
-    model = Sequential([
-        Flatten(input_shape=(28, 28)),
-        Dense(128, activation='relu'),
-        Dense(64, activation='relu'),
-        Dense(10, activation='softmax')
-    ])
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train_cat, epochs=5, verbose=0)
-    return model
+def load_model():
+    return tf.keras.models.load_model("model.h5")
 
-model = train_model()
+model = load_model()
+
+# Load MNIST test data
+(_, _), (x_test, y_test) = mnist.load_data()
+x_test = x_test / 255.0
 
 # Choose test image
 index = st.slider("Pick a test image (0–9999)", 0, 9999, 0)
 st.image(x_test[index], width=150, caption=f"Actual: {y_test[index]}")
 
 # Predict digit
-prediction = model.predict(np.array([x_test[index]]))
+img_input = np.array([x_test[index]])
+prediction = model.predict(img_input)
 predicted_digit = np.argmax(prediction)
 
 # Show prediction
